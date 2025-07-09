4b8b242da6387ec7d936064d8cc42f3ae5d50998
RohaidAhmed <rohaidahmed123@gmail.com>
2025-07-09 16:31:32 +0500

    first commit

--- src/app/(home)/components/BentoGridDemo.tsx
+++ src/app/(home)/components/BentoGridDemo.tsx
@@ -1,14 +1,8 @@
-import { cn } from "@/lib/utils";
 import React from "react";
 import { BentoGrid, BentoGridItem } from "@/components/ui/bento-grid";
 import {
-  IconArrowWaveRightUp,
-  IconBoxAlignRightFilled,
-  IconBoxAlignTopLeft,
   IconClipboardCopy,
   IconFileBroken,
-  IconSignature,
-  IconTableColumn,
 } from "@tabler/icons-react";
 import Link from "next/link";
 import Image from "next/image";
--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,10 +1,7 @@
-import Image from "next/image";
 import { SpotlightPreview } from "./components/SpotlightPreview";
 import { TabsDemo } from "./components/TabsDemo";
 import { BentoGridDemo } from "./components/BentoGridDemo";
 import { BackgroundBeamsDemo } from "./components/BackgroundBeamsDemo";
-import { cn } from "@/lib/utils";
-import { LampDemo } from "./components/LampDemo";
 
 export default function Home() {
   return (
