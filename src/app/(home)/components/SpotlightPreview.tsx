c67e34bbf0ab417021bb6ec54643eb48b324abe3
RohaidAhmed <rohaidahmed123@gmail.com>
2025-09-04 13:15:49 +0500

    adjusted responsiveness

--- src/app/(home)/components/SpotlightPreview.tsx
+++ src/app/(home)/components/SpotlightPreview.tsx
@@ -1,16 +1,17 @@
 import React from "react";
 import { Spotlight } from "@/components/ui/Spotlight";
+import { cn } from "@/lib/utils";
 
 
 export function SpotlightPreview() {
   return (
-    <div className="relative flex h-screen w-full overflow-hidden rounded-md bg-black/[0.96] antialiased md:items-center md:justify-center">
-      {/* <div
+    // <div className="relative flex h-screen w-full overflow-hidden rounded-md bg-black/[0.96] antialiased items-center justify-center">
+      <div
         className={cn(
-          "pointer-events-none absolute inset-0 [background-size:40px_40px] select-none",
-          "[background-image:linear-gradient(to_right,#171717_1px,transparent_1px),linear-gradient(to_bottom,#171717_1px,transparent_1px)]",
+          "pointer-events-none relative  select-none overflow-hidden h-screen justify-center items-center flex antialiased w-full rounded-md",
+          // "[background-image:linear-gradient(to_right,#171717_1px,transparent_1px),linear-gradient(to_bottom,#171717_1px,transparent_1px)]",
         )}
-      /> */}
+      >
 
       <Spotlight
         className="-top-40 left-0 md:-top-20 md:left-60"
@@ -20,7 +21,7 @@ export function SpotlightPreview() {
         <h1 className="bg-opacity-50 bg-gradient-to-b from-neutral-50 to-neutral-400 bg-clip-text text-center text-4xl font-bold text-transparent md:text-7xl">
           Rohaid Ahmed Mirza
         </h1>
-        <h2 className="bg-opacity-50 bg-gradient-to-b from-neutral-50 to-neutral-400 bg-clip-text text-center text-4xl font-bold text-transparent md:text-5xl mt-1">
+        <h2 className="bg-opacity-50 bg-gradient-to-b from-neutral-50 to-neutral-400 bg-clip-text text-center text-3xl font-bold text-transparent md:text-5xl mt-1">
           Full Stack Engineer
         </h2>
         <p className="mx-auto mt-4 max-w-lg text-center text-base font-normal text-neutral-300">
